7578569661d28dcfae5c6468aaeddc9f3bb435e1
Héctor Ramos <hector@parse.com>
2012-10-05 16:28:24 -0700

    Update to Parse.framework 1.1.12

--- PushNotificationTutorial/ViewController.m
+++ PushNotificationTutorial/ViewController.m
@@ -2,56 +2,30 @@
 //  Copyright (c) 2012 Parse. All rights reserved.
 
 #import "ViewController.h"
-#import "Parse/Parse.h"
+#import <Parse/Parse.h>
 
 @implementation ViewController
 
-/* Touch handler for the button */
-- (IBAction)broadcastPushNotification:(id)sender 
-{
-    [PFPush sendPushMessageToChannelInBackground:@"" withMessage:@"Hello World!"];
-}
+#pragma mark - UIViewController
 
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self setTitle:@"Parse Push Notifications"];
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+
+
+#pragma mark - ()
+
+/* Touch handler for the button */
+- (IBAction)broadcastPushNotification:(id)sender  {
+    [PFPush sendPushMessageToChannelInBackground:@"" withMessage:@"Hello World!"];
+}
+
 
 @end
